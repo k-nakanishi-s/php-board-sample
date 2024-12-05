b6f9bb18ec9c7a0f552544e53ebacd9f7e644aec
Kaede Nakanishi <kaede161221@gmail.com>
2024-12-05 15:22:56 +0900

    講義中に修正した結果

---
+++ SimpleCalculator.php
@@ -0,0 +1,16 @@
+<?php
+class SimpleCalculator
+{
+    public function __construct(int $initialValue)
+    {
+        $this->total = $initialValue;
+    }
+    public function add(int $value): int
+    {
+        $this->total += $value;
+        return $this->total;
+    }
+}
+
+$calculator = new SimpleCalculator(10);
+echo $calculator->add(5);
